1ed059e9da3af2d6a9e119ea36d1136766f359da
Tim <39254058+sunjaeyoon@users.noreply.github.com>
2021-06-22 00:18:26 -0400

    Frontend and application

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -18,5 +18,13 @@ contract TodoList {
   function createTask(string memory _content) public {
     taskCount ++;
     tasks[taskCount] = Task(taskCount, _content, false);
+    emit TaskCreated(taskCount, _content, false);
+  }
+  
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
   }
 }
